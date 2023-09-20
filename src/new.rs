b06a6d87cb702c7f3064e15cf020c404f08d2bb6
Adam McKee <adam.be.g84d@gmail.com>
2023-09-20 15:16:52 -0500

    rename mapping to index

--- src/new.rs
+++ src/new.rs
@@ -3,12 +3,12 @@ use std::io::Write;
 use crate::util::error_exit;
 
 pub fn create_new_resource() {
-    let filename = "mappings/mapping.json";
+    let filename = "mappings/index.json";
     let mut file = match std::fs::File::open(filename) {
         Ok(f) => f,
         Err(err) => error_exit(format!("error creating file {filename}: {err}")),
     };
-    match file.write_all(include_bytes!("mapping.json")) {
+    match file.write_all(include_bytes!("index.json")) {
         Ok(_) => println!("mappings/mapping.json created"),
         Err(err) => error_exit(format!("error writing to file {filename}: {err}")),
     }
